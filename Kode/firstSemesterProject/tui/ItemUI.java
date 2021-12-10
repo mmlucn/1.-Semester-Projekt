fd26f7b70daa823a5bd54be352b0bc60a0cdb192
t3nta <10407524@ucn.dk>
2021-12-10 09:53:51 +0100

    lmao

---
+++ Kode/firstSemesterProject/tui/ItemUI.java
@@ -0,0 +1,39 @@
+package tui;
+import controller.*;
+
+/**
+ * Write a description of class ItemUI here.
+ *
+ * @author ()
+ * @version ()
+ */
+public class ItemUI
+{
+    // instance variables - replace the example below with your own
+    private ItemController itemController;
+
+    /**
+     * Constructor for objects of class ItemUI
+     */
+    public ItemUI()
+    {
+        // initialise instance variables
+        
+    }
+    
+    public void createItem(){
+        
+    }
+    
+    public void findItem(){
+        
+    }
+    
+    public void updateItem(){
+        
+    }
+    
+    public void deleteItem(){
+        
+    }
+}
