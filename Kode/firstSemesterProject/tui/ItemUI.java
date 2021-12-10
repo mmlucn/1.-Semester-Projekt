a57b2a770eadf0e1392e8c18ca61d1ec014caed1
malthemorsing <93257348+mmlucn@users.noreply.github.com>
2021-12-10 11:25:14 +0100

    startet på ui

--- Kode/firstSemesterProject/tui/ItemUI.java
+++ Kode/firstSemesterProject/tui/ItemUI.java
@@ -1,12 +1,7 @@
 package tui;
 import controller.*;
+import java.util.Scanner;
 
-/**
- * Write a description of class ItemUI here.
- *
- * @author ()
- * @version ()
- */
 public class ItemUI
 {
     // instance variables - replace the example below with your own
@@ -15,25 +10,53 @@ public class ItemUI
     /**
      * Constructor for objects of class ItemUI
      */
+    private Scanner scanner = new Scanner(System.in);   
     public ItemUI()
     {
         // initialise instance variables
-        
+        boolean shouldRun = true;
+        while (shouldRun){
+            print("1. Create a new item");
+            print("2. Find an item");
+            print("3. Update an item");
+            print("4. Delete an item");
+            int choice = scanner.nextInt();
+            if (choice == 1)
+                createItem();
+            else if (choice == 2)
+                findItem();
+            else if (choice == 3)
+                updateItem();
+            else if (choice == 4)
+                deleteItem();
+            else
+                print("Invalid choice");
+        }
     }
-    
+
     public void createItem(){
+        String itemName;
         
+        boolean shouldRun = true;
+        while (shouldRun){
+            print("Item name:");
+            itemName = scanner.next();
+        }
     }
-    
+
     public void findItem(){
-        
+
     }
-    
+
     public void updateItem(){
-        
+
     }
-    
+
     public void deleteItem(){
-        
+
+    }
+
+    public void print(String text){
+        System.out.println(text);
     }
 }
